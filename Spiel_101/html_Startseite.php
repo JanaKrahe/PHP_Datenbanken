343f86c92722abbba3ebdf60184021d4e23907f7
Krahe.Jana <j.krahe@freenet.de>
2017-04-12 21:04:24 +0200

    CSS und Startseite probe

---
+++ Spiel_101/html_Startseite.php
@@ -0,0 +1,50 @@
+<html>
+
+	  <head>
+      <title>
+        Anmeldeseite_101
+      </title>
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/meinecss.css">
+      <meta charset="UTF-8">
+	  </head>
+
+	  <body class="testklasse">
+      <div id="kopfbereich">
+<p>Spiel 101</p>
+</div>
+
+<div id="steuerung">
+Steuerung
+</div>
+
+<div id="schatten">
+Hier kommt der Schatten später
+</div>
+
+<div id="inhalt">
+Der eigentliche Inhaltsbereich
+</div>
+
+      <p>
+        Anmeldung für das Spiel 101
+      </p>
+      <p>Wird für Absätze benutzt.</p>
+	     <b>Schreibt den Text in Fettschrift.</b>
+	      <h1>Anmeldung</h1>
+	       <h2>Sub-Überschrift</h2>
+	        <h3>Sub-Sub-Überschrift</h3>
+
+          <form method="post" action="test.php">
+              <!-- Formular hier -->
+              <input type="text" name="usrname" value="Benutzername">
+              <input type="password" name="pass" value="Passwort">
+              <input type="submit" name="submit" value="Abschicken">
+          </form>
+
+          <a href="page2.htm">Zur Seite 2</a>
+
+          <a href="https://static1.wall-art.de/out/pictures/generated/product/2/780_542_80/Wandtattoo-Wuerfel-einzeln.jpg"><img src="logo.png" /></a>
+	  </body>
+
+	</html>
