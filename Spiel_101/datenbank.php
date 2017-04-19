e2e8148f1512c496fd83e7afce1907f1380a68a0
Krahe.Jana <j.krahe@freenet.de>
2017-04-19 15:27:07 +0200

    benutzer101 zu spiel101

--- Spiel_101/datenbank.php
+++ Spiel_101/datenbank.php
@@ -5,7 +5,7 @@
 class DatenbankAufrufe
 {
   function datenbankVerbinden(){
-    $pdo = new PDO('mysql:host=localhost;dbname=benutzer101','root','');
+    $pdo = new PDO('mysql:host=localhost;dbname=spiel101','root','');
     if (!$pdo) {
         die("Connection failed: " . mysqli_connect_error());
     }
@@ -13,7 +13,7 @@ class DatenbankAufrufe
 
   function benutzerAnlegen()
   {
-  $pdo = new PDO('mysql:host=localhost;dbname=benutzer101','root','');
+  $pdo = new PDO('mysql:host=localhost;dbname=spiel101','root','');
     $neuer_user = array();
     $neuer_user['email'] = $_POST['email'];
     $neuer_user['passwort'] = $_POST['passwort'];
@@ -26,7 +26,7 @@ class DatenbankAufrufe
   }
 
   function existBenutzer(){
-    $pdo = new PDO('mysql:host=localhost;dbname=benutzer101','root','');
+    $pdo = new PDO('mysql:host=localhost;dbname=spiel101','root','');
     $sqlStatement = "SELECT * FROM user101 WHERE email = '".$_POST['email']."'";
     $stmt = $pdo->prepare($sqlStatement);
     $stmt->execute();
@@ -39,7 +39,7 @@ class DatenbankAufrufe
   }
 
 function passwortAuslesen(){
-  $pdo = new PDO('mysql:host=localhost;dbname=benutzer101','root','');
+  $pdo = new PDO('mysql:host=localhost;dbname=spiel101','root','');
   $sqlStatement = "SELECT passwort FROM user101 WHERE email = '".$_POST['email']."'";
   $stmt = $pdo->prepare($sqlStatement);
   $stmt->execute();
@@ -48,7 +48,7 @@ function passwortAuslesen(){
 }
 
 function benutzerLoeschen(){
-  $pdo = new PDO('mysql:host=localhost;dbname=benutzer101','root','');
+  $pdo = new PDO('mysql:host=localhost;dbname=spiel101','root','');
   $sqlStatement = "DELETE FROM user101 WHERE email = '".$_POST['email']."'";
   $stmt = $pdo->prepare($sqlStatement);
   $stmt->execute();
@@ -56,7 +56,7 @@ function benutzerLoeschen(){
 }
 
   function kennwortAendern($hashPasswort){
-    $pdo = new PDO('mysql:host=localhost;dbname=benutzer101','root','');
+    $pdo = new PDO('mysql:host=localhost;dbname=spiel101','root','');
     $sqlStatement = "UPDATE user101 SET passwort = '".$hashPasswort."' WHERE email = '".$_POST['email']."'";
     $stmt = $pdo->prepare($sqlStatement);
     $stmt->execute();
@@ -64,7 +64,7 @@ function benutzerLoeschen(){
   }
 
   function benutzernameAuslesen(){
-    $pdo = new PDO('mysql:host=localhost;dbname=benutzer101','root','');
+    $pdo = new PDO('mysql:host=localhost;dbname=spiel101','root','');
     $sqlStatement = "SELECT benutzername FROM user101 WHERE email = '".$_POST['email']."'";
     $stmt = $pdo->prepare($sqlStatement);
     $stmt->execute();
