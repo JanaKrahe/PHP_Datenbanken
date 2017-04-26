3702f23426653a7c077d04a9717bb4e18e206e87
lalle619 <l.korthing@gmail.com>
2017-04-27 00:07:35 +0200

    Navbar die Erste

---
+++ Spiel_101/testnav.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Das neueste kompilierte und minimierte CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Optionales Theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+    <!-- Das neueste kompilierte und minimierte JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <!-- Website Font style  -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/meinecss.css">
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+
+    <script src="regler.js"> </script>
+    <title>Spiel101</title>
+  </head>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <label class="navbar-brand">Spiel 101</label>
+        <a class="navbar-brand" ><small>Spielen</small></a>
+      </div>
+      <!--<div class="nav navbar-nav navbar-collapse">
+        <a class="navbar-brand" ><small>Spielen</small></a>
+      </div>-->
+      <ul class="nav navbar-nav navbar-right">
+        <li><a><span class="glyphicon glyphicon-user"></span> Signed in as Mark Otto</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+        <span class="glyphicons glyphicons-dice-6"></span>
+      </ul>
+    </div>
+  </nav>
+</body>
+</html>
